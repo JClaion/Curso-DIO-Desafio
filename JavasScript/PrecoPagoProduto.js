9ad1622551ca748af6639922bf2a4122536a40f4
JClaion <Joseclaionmartins@gmail.com>
2023-12-31 19:50:44 -0400

    DOCS: Update PrecoPagoProduto.js

--- JavasScript/PrecoPagoProduto.js
+++ JavasScript/PrecoPagoProduto.js
@@ -1,17 +1,24 @@
-const valorTotal = 100;
-const tipoDePagamento = "Nada";
-const totalDeVezesPagamento = 3;
-
-if (tipoDePagamento === "Débito"){
-    const valorFinal =(valorTotal) - valorTotal * 0.1;
-    console.log(valorFinal);
-} else if(tipoDePagamento === "Dinheiro" || tipoDePagamento === "Pix"){
-    const valorFinal =(valorTotal) - valorTotal * 0.15;
-    console.log(valorFinal);
-} else if (totalDeVezesPagamento === 2) {
-    const valorFinal =(valorTotal)
-    console.log(valorFinal);
-} else {
-    const valorFinal =(valorTotal) + valorTotal * 0.1;
-    console.log(valorFinal);
+function aplicarDesconto(valor, desconto) {
+    return (valor - (valor * (desconto/100)));
 }
+
+function aplicarJuros(valor, juros){
+    return (valor + (valor * (juros/100)));
+}
+
+function main(){
+    const valorTotal = 100;
+    const tipoDePagamento = "Credito";
+    const totalDeVezesPagamento = 2;
+
+    if (tipoDePagamento === "Débito"){
+        console.log(aplicarDesconto(valorTotal, 10));
+    } else if(tipoDePagamento === "Dinheiro" || tipoDePagamento === "Pix"){
+        console.log(aplicarDesconto(valorTotal, 15));
+    } else if (totalDeVezesPagamento === 2) {
+        console.log(valorTotal);
+    } else {
+        console.log(aplicarJuros(valorTotal, 10));
+    }
+}
+main();
